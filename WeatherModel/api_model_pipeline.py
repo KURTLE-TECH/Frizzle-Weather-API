36dced1bb367d2383c39134ff329637e8617ca9d
Rishith Bhowmick <brishi00@gmail.com>
2021-05-11 11:32:00 +0530

    weather model without colors working after changes

--- WeatherModel/api_model_pipeline.py
+++ WeatherModel/api_model_pipeline.py
@@ -19,38 +19,58 @@ class Model_Pipeline(object):
         
 
     def time_feat(self,time):
-        self.feat = list()
+        self.feat = dict()
         temp_time = pd.DataFrame([time], columns = ['datetime'])
         temp_date = pd.to_datetime(temp_time['datetime'])
-        self.feat.append(temp_date.dt.hour[0])
-        self.feat.append(temp_date.dt.dayofweek[0])
-        self.feat.append(temp_date.dt.quarter[0])
-        self.feat.append(temp_date.dt.month[0])
-        self.feat.append(temp_date.dt.dayofyear[0])
-        self.feat.append(temp_date.dt.day[0])
-        self.feat.append(temp_date.dt.weekofyear[0])
+
+        # self.feat.append(temp_date.dt.month[0])
+        # self.feat.append(temp_date.dt.dayofweek[0])
+        # self.feat.append(temp_date.dt.weekofyear[0])
+        # if temp_date.dt.hour[0] in range(5,12):
+        #     self.feat.append(0)
+        # elif temp_date.dt.hour[0] in [12,13,14,15]:
+        #     self.feat.append(1)
+        # elif temp_date.dt.hour[0] in [16,17,18,19]:
+        #     self.feat.append(2)
+        # elif temp_date.dt.hour[0] in [20,21,22,23,0,1,2,3,4]:
+        #     self.feat.append(3)
+        # self.feat.append(temp_date.dt.hour[0])
+        # self.feat.append(temp_date.dt.dayofyear[0])
+        # self.feat.append(temp_date.dt.day[0])
+        # self.feat.append(temp_date.dt.quarter[0])
+        
         # print(type(temp_date.dt.hour[0]))
+        
+        self.feat['hour'] = [temp_date.dt.hour[0]]
+        self.feat['dayofweek'] = [temp_date.dt.dayofweek[0]]
+        self.feat['quarter'] = [temp_date.dt.quarter[0]]
+        self.feat['month'] = [temp_date.dt.month[0]]
+        self.feat['dayofyear'] = [temp_date.dt.dayofyear[0]]
+        self.feat['dayofmonth'] = [temp_date.dt.day[0]]
+        self.feat['weekofyear'] = [temp_date.dt.weekofyear[0]]
         if temp_date.dt.hour[0] in range(5,12):
-            self.feat.append(0)
+            self.feat['Time_Of_Day'] = [0]
         elif temp_date.dt.hour[0] in [12,13,14,15]:
-            self.feat.append(1)
+            self.feat['Time_Of_Day'] = [1]
         elif temp_date.dt.hour[0] in [16,17,18,19]:
-            self.feat.append(2)
+            self.feat['Time_Of_Day'] = [2]
         elif temp_date.dt.hour[0] in [20,21,22,23,0,1,2,3,4]:
-            self.feat.append(3)
+            self.feat['Time_Of_Day'] = [3]
+
+        self.feat = pd.DataFrame.from_dict(self.feat)
         
     def get_feat(self):
-        # print(np.array(self.feat))
-        temperature = self.temp_model.predict(np.array(self.feat).reshape(1,8))
-        self.feat.append(temperature[0])
-        # print(np.array(self.feat))
-        humidity = self.humid_model.predict(np.array(self.feat).reshape(1,9))
-        self.feat.append(humidity[0])
-        # print(np.array(self.feat))
-        pressure = self.press_model.predict(np.array(self.feat).reshape(1,10))
-        self.feat.append(pressure[0])
-        #print(self.feat)
-        # print(np.array(self.feat))
+        
+        temperature = self.temp_model.predict(self.feat)
+        
+        # self.feat['Temperature'] = temperature[0]   
+        self.feat.insert(7,"Temperature",[temperature[0]],True)
+        humidity = self.humid_model.predict(self.feat)
+        self.feat.insert(7,"Humidity",[humidity[0]],True)
+        pressure = self.press_model.predict(self.feat)
+
+        self.feat.insert(7,"Pressure",[pressure[0]],True)
+        print(self.feat)
         #wind_speed = self.wind_speed_model.predict(self.feat)
         #self.feat.append(wind_speed)
 
@@ -60,25 +80,29 @@ class Model_Pipeline(object):
         final_color = colour_init.NNperc()
         #cloud_type_init = cloud_classification.Cloud_Classification(self.image)
         #final_cloud_type = cloud_type_init.cloud_classify()
-        final_color = np.array(list(final_color.values())).reshape(-1,1)
-        pca_single_color = pca.fit_transform(final_color)
-        self.feat.append(pca_single_color)
+        # final_color = np.array(list(final_color.values())).reshape(-1,1)
+        # print(final_color)
+        pca_single_color = pca.fit_transform([final_color.values()])
+        print(pca_single_color)
+        self.feat.insert(11,"colors",pca_single_color,True)
         #Algorithm to pca into single color value
 
     def forecast_weath(self,time):
         #Forecast general weather conditions for a long term perspective
         self.time_feat(time)
         self.get_feat()
-        self.feat.append(np.nan)
+        self.feat.insert(11,"colors",np.nan,True)
         # print(np.array(self.feat).shape)
-        weather_desc = self.weath_model.predict(np.array(self.feat).reshape(1,12))
+        self.feat = self.feat[['hour', 'dayofweek', 'quarter', 'month', 'dayofyear', 'dayofmonth', 'weekofyear', 'Humidity', 'Pressure', 'Temperature', 'Time_Of_Day', 'colors']]
+        weather_desc = self.weath_model.predict(self.feat)
         return weather_desc
 
     def forecast_micro_weath(self,time):
         self.time_feat(time)
         self.get_feat()
         self.get_feat_from_image(self.image) #room for optimisation ; is repeatedly called
-        weather_desc = self.weath_model.predict(np.array(self.feat).reshape(1,12))
+        self.feat = self.feat[['hour', 'dayofweek', 'quarter', 'month', 'dayofyear', 'dayofmonth', 'weekofyear', 'Humidity', 'Pressure', 'Temperature', 'Time_Of_Day', 'colors']]
+        weather_desc = self.weath_model.predict(self.feat)
         return weather_desc
         #Insert conditions for forecast of micro-weather conditions
         #Get difference in timing in order to write algorithm accordingly
